79288f21874e3a6fff4d76461fcf39a2bd3a942b
SeptimiuPop <septimiu.pop@kcl.ac.uk>
2022-03-08 23:37:01 +0000

    Git ignore and project cleanup

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ cd bin
 echo "Building the enviroment..."
 echo
 
-g++ -c ../src/*.cpp -I ../External/SFML/include
-g++ *.o -o main.exe -L ../External/SFML/lib -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-system # to link.
+g++ -c ../src/*.cpp 
+g++ *.o -o main.exe -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-system # to link.
 
 ./main.exe 
 
